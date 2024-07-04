1868dad6fe0caae6674c6ff89bdf056269444620
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2024-07-04 19:04:57 +0200

    Update AtomLaser.py

--- AtomLaser.py
+++ AtomLaser.py
@@ -22,7 +22,7 @@
 #   ** : 
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
-#   (the "Software" bec_symmetry_breaking.py), to deal in the Software without restriction, including without limitation the rights to use, 
+#   (the "Software" AtomLaser.py), to deal in the Software without restriction, including without limitation the rights to use, 
 #   copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
 #   furnished to do so, subject to the following conditions:
 # 
